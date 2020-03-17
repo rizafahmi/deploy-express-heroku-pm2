f36bd877456b01c7ca539818466bed8a589924bf
Riza Fahmi <rizafahmi@gmail.com>
2020-03-17 10:06:46 +0700

    Change port

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('deploy this!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running.');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
