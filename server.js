e349c73d8e62a838d1f6c65a240cbffa7d4734b7
Riza Fahmi <rizafahmi@gmail.com>
2020-03-17 13:18:36 +0700

    Use pm2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongodb = require('mongodb').MongoClient;
+const faker = require('faker');
 
 const app = express();
 
@@ -7,13 +8,26 @@ const PORT = process.env.PORT || 3000;
 const url =
   process.env.MONGODB_URL || 'mongodb://localhost:27017/learning-mongodb';
 
+// mongodb+srv://riza:riza@cluster0-ooomx.mongodb.net/test
+
 app.get('/', async (req, res) => {
   try {
     const client = await mongodb.connect(url, { useUnifiedTopology: true });
     const db = client.db('learning-mongodb');
     const collection = db.collection('pets');
     const pets = await collection.find({}).toArray();
-    res.send(pets);
+    if (pets.length > 0) {
+      res.json(pets);
+    } else {
+      for (let i = 0; i < 100; i++) {
+        collection.insertOne({
+          name: faker.name.findName(),
+          species: faker.company.companyName()
+        });
+        console.log('Insert');
+        res.json({ message: 'Data insert finish.' });
+      }
+    }
   } catch (err) {
     console.log(err.toString());
     res.json({ err: err });
