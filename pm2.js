f38f6e0e3530fa6f7376278b277b90f3424d7406
Riza Fahmi <rizafahmi@gmail.com>
2020-03-17 14:44:01 +0700

    use pm2 script

---
+++ pm2.js
@@ -0,0 +1,44 @@
+const pm2 = require('pm2');
+
+const instances = process.env.WEB_CONCURRENCY || -1;
+const maxMemory = process.env.WEB_MEMORY || 512;
+
+console.log(`Starting server with ${instances} instances.`);
+console.log(`You should see ${instances} random numbers logged.`);
+
+const options = {
+  name: 'deploy-express',
+  script: 'server.js',
+  exec_mode: 'cluster',
+  instances: instances,
+  max_memory_restart: `${maxMemory}M`
+};
+
+pm2.connect((err) => {
+  if (err) {
+    console.error(err);
+    process.exit(2);
+  }
+
+  pm2.start(options, (err) => {
+    if (err)
+      return console.error(
+        'Error while launching applications',
+        err.stack || err
+      );
+    console.log('PM2 and application has been succesfully started');
+
+    // Display logs in standard output
+    pm2.launchBus((err, bus) => {
+      console.log('[PM2] Log streaming started');
+
+      bus.on('log:out', function(packet) {
+        console.log('[App:%s] %s', packet.process.name, packet.data);
+      });
+
+      bus.on('log:err', function(packet) {
+        console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+      });
+    });
+  });
+});
