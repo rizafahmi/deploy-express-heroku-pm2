12df3be65f6bd0b47fe8d2d37686f76f5944ca32
Riza Fahmi <rizafahmi@gmail.com>
2020-03-17 11:41:07 +0700

    Connect to mongodb atlas

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const mongodb = require('mongodb').MongoClient;
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const url =
+  process.env.MONGODB_URL || 'mongodb://localhost:27017/learning-mongodb';
 
-app.get('/', (req, res) => {
-  res.send('deploy this!');
+app.get('/', async (req, res) => {
+  try {
+    const client = await mongodb.connect(url, { useUnifiedTopology: true });
+    const db = client.db('learning-mongodb');
+    const collection = db.collection('pets');
+    const pets = await collection.find({}).toArray();
+    res.send(pets);
+  } catch (err) {
+    console.log(err.toString());
+    res.json({ err: err });
+  }
 });
 
 app.listen(PORT, () => {
